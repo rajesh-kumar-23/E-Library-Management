691f669a2b78bd45d0137b5759afc61b3e4841c4
Rajesh Kumar <rajeshkumarv.2323@gmail.com>
2022-06-20 13:15:37 +0530

    Added new
    E-library management using c# and ms sql server as database

---
+++ Website/Info Security/userlogin.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Info_Security
+{
+    public partial class userlogin : System.Web.UI.Page
+    {
+
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //Response.Write("<script>alert('Btn Click');</script>");
+
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+
+                }
+                SqlCommand cmd = new SqlCommand("select * from member_master_tbl where member_id='" + TextBox1.Text.Trim() + "' AND password='" + TextBox2.Text.Trim() + "'", con);
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        Response.Write("<script>alert('login successfull');</script>");
+                        Session["username"] = dr.GetValue(8).ToString();
+                        Session["fullname"] = dr.GetValue(0).ToString();
+                        Session["role"] = "user";
+                        Session["status"] = dr.GetValue(10).ToString();
+                    }
+
+                    Response.Redirect("homepage.aspx");
+
+                }
+                else
+                {
+                    Response.Write("<script>alert('Invalid credentials');</script>");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+
+
+        
+
+    }
+}
